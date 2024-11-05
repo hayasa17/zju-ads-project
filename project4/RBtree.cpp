efa2eaa423c97e8ac4e85728020ca16fb0f98358
hayasa17 <1209060758@qq.com>
2024-11-05 15:18:03 +0800

    fix the time complexiety

--- project4/RBtree.cpp
+++ project4/RBtree.cpp
@@ -3,16 +3,18 @@ using namespace std;
 
 // dp[number][blackpath][isblack]
 const int mod = 1e9 + 7;
-// 第一个动态规划函数，时间O(n^3),空间O(n^2)
+// 第一个动态规划函数，时间O(n^2 logn),空间O(nlogn)
 void DP1(int n) {
   // 初始化动态规划数组，大小为 (n+1) x (n+2) x 2，所有值初始化为0
-  vector dp(n + 1, vector<vector<int>>(n + 2, vector<int>(2, 0)));
+  vector dp(n + 1,
+            vector<vector<int>>((int)(2 * log2(n + 1)) + 1, vector<int>(2, 0)));
   // 初始状态，只有NIL的情况
   dp[0][1][1] = 1;
   // 枚举当前考虑的树的大小
   for (int i = 1; i <= n; ++i) {
     // 枚举blackpath长度
-    for (int j = 1; j <= i + 1; ++j) {
+    int lim = 2 * log2(n + 1);
+    for (int j = 1; j <= lim; ++j) {
       // 枚举左子树大小
       for (int k = 0; k < i; ++k) {
         // 更新当前条件下，根节点为黑色的树的个数
@@ -28,13 +30,13 @@ void DP1(int n) {
       }
     }
   }
-  int ans = 0;
+  int ans = 0, lim = 2 * log2(n + 1);
   // 统计n个节点,blackpath长度为i，根节点为黑色的红黑树数量
-  for (int i = 1; i <= n + 1; ++i) ans = (ans + dp[n][i][1]) % mod;
+  for (int i = 1; i <= lim; ++i) ans = (ans + dp[n][i][1]) % mod;
   cout << ans << endl;
   return;
 }
-// 第二个动态规划函数，时间O(n^3),空间O(n)
+// 第二个动态规划函数，时间O(n^2 logn),空间O(n)
 void DP2(int n) {
   int ans = 0;
   // 初始化动态规划数组，大小为 (n+1) x 2，所有值初始化为0
@@ -43,7 +45,8 @@ void DP2(int n) {
   // 只存在NIL，只存在一个红色节点
   dp[0][1] = dp[1][0] = 1;
   // 遍历每一列
-  for (int j = 2; j <= n + 1; ++j) {
+  int lim = 2 * log2(n + 1);
+  for (int j = 2; j <= lim; ++j) {
     // 遍历每一行
     for (int i = n; i >= 0; --i) {
       dp[i][1] = 0;
@@ -69,7 +72,7 @@ void DP2(int n) {
 }
 /*
   由DP1优化至DP2可以观察到，DP2实际上为一个卷积形式
-  若模数更为理想，可以使用NTT进一步优化至时间O(n^2 logn)
+  若模数更为理想，可以使用NTT进一步优化至时间O(n log^2n)
 */
 int main() {
   int n;
